52cd060bcb894b4f5fc723f66e77054f3bd3ad9d
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-11-08 17:41:08 +0100

    feat: create base server setup
    - Implemented a basic Express server setup to handle incoming HTTP requests
    - Configured middleware for parsing JSON and handling requests
    - Prepared the server for further route integration and middleware additions
    - Ensured the server is ready for testing and initial deployment

---
+++ src/appServer.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import {sequelize} from "./database.js";
+import router from "./routes/index.js";
+
+dotenv.config();
+
+const appServer = express();
+const SERVER_PORT: number = parseInt(process.env.SERVER_PORT ?? "666", 10);
+
+appServer.use(express.json());
+appServer.use(express.urlencoded({extended: true}));
+appServer.use("/api", router);
+
+async function start(): Promise<void> {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync({alter: true});
+    console.log("Connected to database");
+    appServer.listen(SERVER_PORT, () => {
+      console.log(`Server is running on http://localhost:${SERVER_PORT}`);
+    });
+  } catch (error) {
+    console.error("Error connecting to database: ", error);
+    process.exit(1);
+  }
+}
+
+start();
