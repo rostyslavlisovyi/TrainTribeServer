85c856de06c0cc6ecb5bb8c8adff74d72e8347e6
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-11-08 22:43:47 +0100

    feat: add auth route to index.ts
    - Added router.use("/auth", authRoutes) to index.ts for handling authentication routes
    - Integrated authRoutes to manage sign-up and sign-in endpoints
    - Improved project structure by centralizing route management

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import usersRoutes from "./users.routes.js";
+import authRoutes from "./auth.routes.js";
 
 const router = express.Router({mergeParams: true});
 
 router.use("/users", usersRoutes);
+router.use("/auth", authRoutes);
 
 export default router;
