bf2883cb139d15340789b2241e9a979d9815e5f1
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-11-08 22:37:47 +0100

    feat: add routes for signUp and signIn
    - Implemented routes for user signUp and signIn in the authentication module
    - Created POST route for signUp to handle new user registration
    - Added POST route for signIn to authenticate users and generate tokens

---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,106 @@
+import express, {NextFunction, Request, Response} from "express";
+import {handleError} from "../utils/handleError.js";
+import {type Model} from "sequelize";
+
+import UserModel from "../models/user.js";
+
+import {nanoid} from "nanoid";
+import {verifyPassword, hashPassword} from "../utils/passwordUtils.js";
+import jwt from "jsonwebtoken";
+import {ILoginUser, IUser, IUserInput,IRegisterUser} from "../interfaces";
+
+type UserInstance = Model<IUser, IUserInput>;
+
+const authRoutes = express.Router({mergeParams: true});
+
+// @ts-ignore
+authRoutes.post("/signUp", async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    console.log("post");
+    const {username, email, password, location}: IRegisterUser = req.body;
+    const existingUser: UserInstance | null = await UserModel.findOne({
+      where: {
+        email: email
+      }
+    });
+    const hashedPassword: string = await hashPassword(password);
+    if (!existingUser) {
+      console.log("create user");
+      const newUser: IUserInput = {
+        external_id: nanoid(12),
+        username: username,
+        first_name: "",
+        last_name: "",
+        email: email,
+        password: hashedPassword,
+        location: location
+      };
+      await UserModel.create<UserInstance>(newUser);
+      const token: string = jwt.sign(
+        {
+          email: email
+        },
+        process.env.JWT_SECRET ?? "",
+        {
+          expiresIn: "1h"
+        }
+      );
+      console.log("User created");
+      return res.status(201).json({token: token, user: newUser});
+    } else {
+      console.log("User already exists");
+      return res.status(409).json({message: "User already exists"});
+    }
+  } catch (error) {
+    handleError(error);
+    next(error);
+  }
+});
+
+// @ts-ignore
+authRoutes.post("/signIn", async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+
+  try {
+    console.log("post");
+    const {email, password}: ILoginUser = req.body;
+    const user: UserInstance | null = await UserModel.findOne({
+      where: {
+        email: email
+      }
+    });
+    if (!user) {
+      return res.status(401).json({message: "User not found"});
+    }
+    const userPassword: string = user.getDataValue("password");
+    console.log("userPassword", userPassword);
+    console.log("password", password);
+    const passwordMatch: boolean = await verifyPassword(password, userPassword);
+    if (!passwordMatch) {
+      return res.status(401).json({message: "Invalid password"});
+    }
+    const token: string = jwt.sign(
+      {
+        id: user.getDataValue("id"),
+        email: user.getDataValue("email")
+      },
+      process.env.JWT_SECRET ?? "",
+      {
+        expiresIn: "1h"
+      }
+    );
+    return res.status(200).json({token: token});
+  } catch (error) {
+    handleError(error);
+    next(error);
+  }
+});
+
+export default authRoutes;
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,52 +2,14 @@ import express, {NextFunction, Request, Response} from "express";
 import UserModel from "../models/user.js";
 import {handleError} from "../utils/handleError.js";
 import {type Model} from "sequelize";
-import {IUser, IUserInput, IFrontUser} from "../interfaces";
-import {nanoid} from "nanoid";
+import {IUser, IUserInput} from "../interfaces";
+
 import {hashPassword} from "../utils/passwordUtils.js";
 
 type UserInstance = Model<IUser, IUserInput>;
 
 const usersRoutes = express.Router({mergeParams: true});
 
-// @ts-ignore
-usersRoutes.post("/", async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    console.log("post");
-    const {username, email, password, location}: IFrontUser = req.body;
-    const existingUser: UserInstance | null = await UserModel.findOne({
-      where: {
-        email: email
-      }
-    });
-    const hashedPassword: string = await hashPassword(password);
-    if (!existingUser) {
-      console.log("create user");
-      const newUser: IUserInput = {
-        external_id: nanoid(12),
-        username: username,
-        first_name: "",
-        last_name: "",
-        email: email,
-        password: hashedPassword,
-        location: location
-      };
-      await UserModel.create<UserInstance>(newUser);
-      console.log("User created");
-      return res.status(201).json({message: "User created"});
-    } else {
-      console.log("User already exists");
-      return res.status(409).json({message: "User already exists"});
-    }
-  } catch (error) {
-    handleError(error);
-    next(error);
-  }
-});
 // @ts-ignore
 usersRoutes.get("/", async (
   req: Request,
@@ -66,19 +28,19 @@ usersRoutes.get("/", async (
 
 // @ts-ignore
 usersRoutes.delete("/:external_id", async (
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
   try {
     const {external_id} = req.params;
     console.log(external_id, "external_id");
     const user: UserInstance | null = await UserModel.findOne({
-        where: {
-            external_id: external_id
-        }
+      where: {
+        external_id: external_id
+      }
     });
-    console.log('user', user);
+    console.log("user", user);
     if (user) {
       await user.destroy();
       return res.status(200).json({message: "User deleted"});
@@ -93,22 +55,22 @@ usersRoutes.delete("/:external_id", async (
 
 // @ts-ignore
 usersRoutes.put("/:external_id", async (
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
   try {
     const { external_id} = req.params;
     const {
-        password,
-        location,
-        first_name,
-        last_name,
+      password,
+      location,
+      first_name,
+      last_name
     }: IUser = req.body;
     const user: UserInstance | null = await UserModel.findOne({
-        where: {
-            external_id: external_id
-        }
+      where: {
+        external_id: external_id
+      }
     });
     if (user) {
       const hashedPassword: string = await hashPassword(password);
@@ -127,7 +89,6 @@ usersRoutes.put("/:external_id", async (
     next(error);
   }
 });
-
 
 export default usersRoutes;
 
