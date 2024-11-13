4edc86e5ec2961683c83d74887be7f9016924450
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-11-13 23:58:20 +0100

    feat: add training routes to main router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import usersRoutes from "./users.routes.js";
 import authRoutes from "./auth.routes.js";
+import trainingRoutes from "./training.routes.js";
 
 const router = express.Router({mergeParams: true});
 
 router.use("/users", usersRoutes);
 router.use("/auth", authRoutes);
-
+router.use("/training", trainingRoutes);
 export default router;
