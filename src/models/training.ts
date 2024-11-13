f8014c3315eab807e04604a969f0e08d788d7705
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-11-13 23:42:45 +0100

    feat: add Training model to define training data structure

---
+++ src/models/training.ts
@@ -0,0 +1,49 @@
+import {DataTypes} from "sequelize";
+import {sequelize} from "../database.js";
+
+const Training = sequelize.define(
+    "Training",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false,
+            unique: true
+        },
+        external_id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        sport: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        distance: {
+            type: DataTypes.FLOAT,
+            allowNull: false
+        },
+        location: {
+            type: DataTypes.JSON,
+            allowNull: true
+        },
+        createdAt: {
+            type: DataTypes.DATE
+        },
+        updatedAt: {
+            type: DataTypes.DATE
+        }
+    },
+    {
+        timestamps: true,
+        tableName: "Trainings"
+    }
+);
+
+export default Training;
