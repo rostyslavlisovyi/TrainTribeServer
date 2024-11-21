c450243254bb0281ea59c6d60c069672c70e11b4
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-11-21 14:25:44 +0100

    feat: update routes to use singular form for sports and user endpoints

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import sportsRoutes from "./sports.route.js";
+import sportsRoutes from "./sport.route.js";
 import authRoutes from "./auth.routes.js";
+import userRoutes from "./user.routes.js";
 
 const router = express.Router({ mergeParams: true });
 
 router.use("/auth", authRoutes);
-router.use("/sports", sportsRoutes);
-// router.use("/users", usersRoutes);
+router.use("/sport", sportsRoutes);
+router.use("/user", userRoutes);
 // router.use("/training", trainingRoutes);
 export default router;
