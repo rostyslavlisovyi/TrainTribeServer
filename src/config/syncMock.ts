f61e5d5c3723588165e6f60eff9c87a5370874b2
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-11-21 18:29:03 +0100

    fix: update mock data import to use correct file and variable names in syncMock

--- src/config/syncMock.ts
+++ src/config/syncMock.ts
@@ -1,19 +1,19 @@
 import Sport from "../models/MongoDB/sport.model.mongoDB.js";
-import { sports } from "../mocks/sports.js";
+import { sportsMock } from "../mocks/sports.mock.js";
 
 export const syncMock = async (): Promise<void> => {
   console.log("Checking if mock data...");
 
   const existingSports = await Sport.find();
 
-  if (existingSports.length === sports.length) {
+  if (existingSports.length === sportsMock.length) {
     console.log("Data is match, no need to insert mock data.");
     return;
   }
   console.log("Data isn't match, inserting mock data...");
   await Sport.deleteMany();
   console.log("Deleted all data.");
-  for (const sport of sports) {
+  for (const sport of sportsMock) {
     await Sport.create(sport);
   }
   console.log("Mock data inserted.");
