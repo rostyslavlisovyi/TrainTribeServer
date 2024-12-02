f2573f0451f6645649cce76c5496464f3c74e56c
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-12-02 21:08:13 +0100

    chore: update environment variables for application configuration and JWT secret

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -3,7 +3,7 @@ import { JwtPayload } from "jsonwebtoken";
 declare global {
   namespace Express {
     export interface Request {
-      user?: string | JwtPayload & { id: string };
+      user?: string | (JwtPayload & { id: string });
     }
   }
 }
