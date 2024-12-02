f1b71d98a4c512219d2232c33f5e30c03dbe95aa
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-12-02 21:07:30 +0100

    refactor: reorder fields in UserSchema for clarity and maintainability

--- src/models/MongoDB/user.model.mongoDB.ts
+++ src/models/MongoDB/user.model.mongoDB.ts
@@ -3,15 +3,14 @@ import { IUser } from "../../interfaces/user.interfaces";
 
 const UserSchema: Schema = new Schema(
   {
+    email: { type: String, required: true, unique: true },
     username: { type: String },
     first_name: { type: String },
     last_name: { type: String },
-    email: { type: String, required: true, unique: true },
-    sports: [{ type: Schema.Types.ObjectId, ref: "Sport", required: true }],
-    password_hashed: { type: String, required: true },
     image_url: { type: String, required: false },
     latitude: { type: Number, required: false },
     longitude: { type: Number, required: false },
+    sports: [{ type: Schema.Types.ObjectId, ref: "Sport", required: true }],
     training_created: [{ type: Schema.Types.ObjectId, ref: "Training" }],
     training_join: [{ type: Schema.Types.ObjectId, ref: "Training" }]
   },
