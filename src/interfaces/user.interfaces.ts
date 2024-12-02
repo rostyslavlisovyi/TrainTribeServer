bc6c3daa82812b680262e92751c18bf442258247
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-12-02 21:08:25 +0100

    refactor: adjust IUser interface by moving sport field for consistency

--- src/interfaces/user.interfaces.ts
+++ src/interfaces/user.interfaces.ts
@@ -1,14 +1,13 @@
 import { Document, ObjectId } from "mongoose";
 export interface IUser extends Document {
   email: string;
-  password_hashed: string;
   username?: string;
   first_name?: string;
   last_name?: string;
-  sport?: ObjectId[];
   image_url?: string;
   latitude?: number;
   longitude?: number;
+  sport?: ObjectId[];
   training_created?: ObjectId[];
   training_join?: ObjectId[];
 }
