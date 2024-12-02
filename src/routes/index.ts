f117e7dcbe264ac71425f7da3e66d7d2d38c033b
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-12-02 21:08:49 +0100

    refactor: streamline user routes by improving ID validation and response handling

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,9 @@
 import express from "express";
 import sportsRoutes from "./sport.route.js";
-import authRoutes from "./auth.routes.js";
 import userRoutes from "./user.routes.js";
 
 const router = express.Router({ mergeParams: true });
 
-router.use("/auth", authRoutes);
 router.use("/sport", sportsRoutes);
 router.use("/user", userRoutes);
 // router.use("/training", trainingRoutes);
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,88 +1,142 @@
-import express, {Router, Request, Response} from "express";
-import {authenticate} from "../middlewares/auth.middleware.js";
+import express, { Router, Request, Response } from "express";
+import { authenticate } from "../middlewares/auth.middleware.js";
 import UserModel from "../models/MongoDB/user.model.mongoDB.js";
+import { IUser } from "../interfaces/user.interfaces.js";
+import mongoose, { HydratedDocument } from "mongoose";
 
 const userRoute: Router = express.Router();
 
-userRoute.get("/", authenticate, async (req:Request, res:Response):Promise<void> => {
-  try {
-    if (typeof req.user === "object" && req.user !== null && "id" in req.user) {
-      const userId: string = req.user.id as string;
-      console.log("userId", userId);
-      const existingUser = await UserModel.findById(userId);
-        if (!existingUser) {
-            res.status(404).json({message: "USER NOT FOUND"});
-            return;
-        }
-          res.status(200).json({user: existingUser});
+userRoute.get(
+  "/",
+  authenticate,
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { _id } = req.body;
+      if (!mongoose.isValidObjectId(_id)) {
+        res.status(400).json({ message: "Invalid ID format" });
+        return;
+      }
+      const existingUser = await UserModel.findById(_id);
+      if (!existingUser) {
+        res.status(404).json({ message: "USER NOT FOUND" });
+        return;
+      }
+      res.status(200).json({ user: existingUser });
+      return;
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "INTERNAL SERVER ERROR" });
+    }
+  }
+);
+
+userRoute.post(
+  "/",
+  authenticate,
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const {
+        email,
+        username,
+        first_name,
+        last_name,
+        image_url,
+        latitude,
+        longitude,
+        sport
+      } = req.body as IUser;
+      if (!email) {
+        res.status(400).json({ message: "EMAIL IS REQUIRED" });
+        return;
+      }
+      const existingUser: HydratedDocument<IUser> | null =
+        await UserModel.findOne().where("email").equals(email);
+      if (existingUser) {
+        res
+          .status(409)
+          .json({ message: "USER WITH THIS EMAIL ALREADY EXISTS" });
+        return;
+      }
+      const newUser: IUser = new UserModel({
+        email: email,
+        username: username || "",
+        first_name: first_name || "",
+        last_name: last_name || "",
+        image_url: image_url || "",
+        latitude: latitude || 0,
+        longitude: longitude || 0,
+        sport: sport || []
+      });
+      const savedUser: IUser = await newUser.save();
+      res.status(201).json({ user: savedUser });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "INTERNAL SERVER ERROR" });
+    }
+  }
+);
+
+userRoute.put(
+  "/",
+  authenticate,
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { _id } = req.body;
+        if (!mongoose.isValidObjectId(_id)) {
+          res.status(400).json({ message: "INVALID ID FORMAT" });
           return;
-    } else {
-        res.status(401).json({message: "UNAUTHORIZED"});
+        }
+        const updates = Object.keys(req.body).reduce(
+          (acc, key) => {
+            acc[key] = req.body[key];
+            return acc;
+          },
+          {} as Record<string, any>
+        );
+
+        if (Object.keys(updates).length === 0) {
+          res.status(400).json({ message: "NO FIELDS PROVIDED FOR UPDATE" });
+          return;
+        }
+
+        const updatedUser = await UserModel.findByIdAndUpdate(_id, updates, {
+          new: true,
+          runValidators: true
+        });
+        if (!updatedUser) {
+          res.status(404).json({ message: "USER NOT FOUND" });
+          return;
+        }
+        res.status(200).json({ user: updatedUser });
         return;
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "INTERNAL SERVER ERROR" });
     }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({message: "INTERNAL SERVER ERROR"});
   }
-});
+);
 
-userRoute.put("/", authenticate, async (req:Request, res:Response):Promise<void> => {
-  try {
-    if (typeof req.user === "object" && req.user !== null && "id" in req.user) {
-      const userId: string = req.user.id as string;
-      console.log("userId", userId);
-
-      const updates = Object.keys(req.body).reduce((acc, key) => {
-        acc[key] = req.body[key];
-        return acc;
-      }, {} as Record<string, any>);
-
-      if (Object.keys(updates).length === 0) {
-        res.status(400).json({message: "NO FIELDS PROVIDED FOR UPDATE"});
+userRoute.delete(
+  "/",
+  authenticate,
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { _id } = req.body;
+      if (!mongoose.isValidObjectId(_id)) {
+        res.status(400).json({ message: "Invalid ID format" });
         return;
       }
-
-      const updatedUser = await UserModel.findByIdAndUpdate(userId, updates,
-          {
-            new: true,
-            runValidators: true,
-          }
-      );
-      if (!updatedUser) {
-        res.status(404).json({message: "USER NOT FOUND"});
-        return;
-      }
-      res.status(200).json({user: updatedUser});
-      return;
-    } else {
-      res.status(401).json({message: "UNAUTHORIZED"});
-      return;
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({message: "INTERNAL SERVER ERROR"});
-  }
-});
-
-userRoute.delete("/", authenticate, async (req:Request, res:Response):Promise<void> => {
-  try {
-    if (typeof req.user === "object" && req.user !== null && "id" in req.user) {
-      const userId: string = req.user.id as string;
-      console.log("userId", userId);
-      const deletedUser = await UserModel.findByIdAndDelete(userId);
+      const deletedUser = await UserModel.findByIdAndDelete(_id);
       if (!deletedUser) {
-        res.status(404).json({message: "USER NOT FOUND"});
+        res.status(404).json({ message: "USER NOT FOUND" });
         return;
       }
-      res.status(200).json({message: "USER DELETED"});
+      res.status(200).json({ message: "USER DELETED" });
       return;
-    } else {
-      res.status(401).json({message: "UNAUTHORIZED"});
-      return;
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "INTERNAL SERVER ERROR", error });
     }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({message: "INTERNAL SERVER ERROR", error});
   }
-});
+);
 export default userRoute;
