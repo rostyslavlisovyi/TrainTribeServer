ac29e38fd30bb81f270cf31c300c9726d62e9a01
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-12-02 21:07:22 +0100

    refactor: replace custom JWT authentication middleware with express-oauth2-jwt-bearer

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,31 +1,11 @@
-import { Request, Response, NextFunction } from "express";
-import { validateToken } from "../utils/jwt.utils.js";
+import dotenv from "dotenv";
+import * as process from "process";
+import { auth } from "express-oauth2-jwt-bearer";
 
-export const authenticate = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-): Promise<void> => {
-  try {
-    const authHeader = req.headers.authorization;
-    const token = authHeader?.split(" ")[1];
+dotenv.config();
 
-    if (!token) {
-      res.status(401).json({ message: "UNAUTHORIZED" });
-      return;
-    }
-
-    const decodedToken = validateToken(token);
-
-    if (!decodedToken) {
-      res.status(401).json({ message: "INVALID TOKEN" });
-      return;
-    }
-
-    req.user = decodedToken;
-    next();
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "INTERNAL SERVER ERROR", error });
-  }
-};
+export const authenticate = auth({
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}`,
+  tokenSigningAlg: "RS256"
+});
