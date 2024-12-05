a0313cb9e6e6fa226048516a002190884eb73789
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-12-05 15:48:19 +0100

    refactor: enhance Sequelize configuration with dynamic logging and connection pooling

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -3,13 +3,22 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const processingMode = process.env.NODE_ENV;
+
 export const sequelize = new Sequelize(
   process.env.MYSQL_DB_NAME ?? "",
   process.env.MYSQL_DB_USER ?? "",
   process.env.MYSQL_DB_PASSWORD ?? "",
   {
-    host: process.env.MYSQL_DB_HOST ?? "localhost",
+    host: process.env.MYSQL_DB_HOST || "localhost",
     dialect: "mysql",
-    logging: false
+    logging: processingMode === "development"? console.log : false,
+    pool: {
+        max: 10,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    }
   }
 );
+
