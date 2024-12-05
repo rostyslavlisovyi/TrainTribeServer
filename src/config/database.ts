4f8d2f8e230a8787bbd8f0223ca67e130e541c4b
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-12-05 15:24:23 +0100

    refactor: improve database connection handling with error logging and process exit

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,19 +1,25 @@
 import mongoose from "mongoose";
 import { sequelize } from "./sequelize.js";
+import * as process from "process";
 
 type DBType = "mysql" | "mongodb";
 export const connectDB = async (): Promise<void> => {
   const dbType: DBType = process.env.DB_TYPE as DBType;
-
-  if (dbType === "mysql") {
-    console.log("Connecting to MySQL database...");
-    await sequelize.authenticate();
-    console.log("Connected to MySQL database.");
-  } else if (dbType === "mongodb") {
-    console.log("Connecting to MongoDB database...");
-    await mongoose.connect(process.env.MONGO_DB_URI ?? "");
-    console.log("Connected to MongoDB database.");
-  } else {
-    throw new Error(`Unsupported DB_TYPE: ${dbType}`);
+  try {
+    if (dbType === "mysql") {
+      console.log("Connecting to MySQL database...");
+      await sequelize.authenticate();
+      console.log("Connected to MySQL database.");
+    } else if (dbType === "mongodb") {
+      console.log("Connecting to MongoDB database...");
+      const mongoURI = process.env.MONGO_DB_URI;
+      await mongoose.connect(mongoURI ?? "");
+      console.log("Connected to MongoDB database.");
+    } else {
+      console.error(`Unsupported DB_TYPE: ${dbType}`);
+    }
+  } catch (error) {
+    console.error(`Error connection to ${dbType} database`, error);
+    process.exit(1);
   }
 };
