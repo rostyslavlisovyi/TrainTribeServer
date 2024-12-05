d883babd68d45f07006e71f46aef82b74262b351
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-12-05 16:05:06 +0100

    refactor: improve syncMock function with error handling and bulk insert for mock data

--- src/config/syncMock.ts
+++ src/config/syncMock.ts
@@ -2,19 +2,21 @@ import Sport from "../models/MongoDB/sport.model.mongoDB.js";
 import { sportsMock } from "../mocks/sports.mock.js";
 
 export const syncMock = async (): Promise<void> => {
-  console.log("Checking if mock data...");
+  try {
+    console.log("Checking if mock data...");
+    const existingSports = await Sport.find();
 
-  const existingSports = await Sport.find();
-
-  if (existingSports.length === sportsMock.length) {
-    console.log("Data is match, no need to insert mock data.");
-    return;
+    if (existingSports.length === sportsMock.length) {
+      console.log("Data is match, no need to insert mock data.");
+      return;
+    }
+    console.log("Data isn't match, inserting mock data...");
+    await Sport.deleteMany();
+    console.log("Deleted all data.");
+    await Sport.insertMany(sportsMock);
+    console.log("Mock data inserted.");
+  } catch (error) {
+    console.error("Error inserting mock data", error);
+    process.exit(1);
   }
-  console.log("Data isn't match, inserting mock data...");
-  await Sport.deleteMany();
-  console.log("Deleted all data.");
-  for (const sport of sportsMock) {
-    await Sport.create(sport);
-  }
-  console.log("Mock data inserted.");
 };
