319fa681ed9462b592d2778f7cb9131b1ff90d88
Rostyslav Lisovyi <rokokos86@protonmail.com>
2025-03-10 17:59:08 +0100

    fix: fix method base routh in city routes

--- src/routes/city.routes.ts
+++ src/routes/city.routes.ts
@@ -7,7 +7,7 @@ const cityRoute: Router = express.Router();
 
 const cityController = container.resolve<CityController>("cityController");
 
-cityRoute.post("/", (req, res) => cityController.getAll(req, res));
+cityRoute.get("/", (req, res) => cityController.getAll(req, res));
 
 cityRoute.get("/:id", (req, res) => cityController.get(req, res));
 
