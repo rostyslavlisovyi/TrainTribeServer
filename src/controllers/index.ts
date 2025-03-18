69bb144c38c797a9c4997133473ded9dca0c14d3
Rostyslav Lisovyi <rokokos86@protonmail.com>
2025-03-18 19:32:18 +0200

    Update index files to include training service and controller exports

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,3 +2,4 @@ export * from "./base.controller.js";
 export * from "./city.controller.js";
 export * from "./upload.controller.js";
 export * from "./user.controller.js";
+export * from "./training.controller.js";
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,12 @@ import express from "express";
 import userRoutes from "./user.routes.ts";
 import uploadRoute from "./upload.route.ts";
 import cityRoutes from "./city.routes.ts";
+import trainingRoutes from "./training.routes.ts";
 const router = express.Router({ mergeParams: true });
 
 router.use("/user", userRoutes);
 router.use("/upload", uploadRoute);
 router.use("/city", cityRoutes);
-// router.use("/training", trainingRoutes);
+router.use("/training", trainingRoutes);
 
 export default router;
