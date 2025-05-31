763213566015931ffbf5859c65e132a1b4fb5282
Rostyslav Lisovyi <lisovyi.rostyslav@gmail.com>
2025-05-31 18:59:32 +0200

    refactor: rename reviewPoints to review_points in user model and update documentation

--- src/models/MongoDB/user.model.ts
+++ src/models/MongoDB/user.model.ts
@@ -65,7 +65,7 @@ const UserSchema: Schema = new Schema(
       }
     ],
     training_points: { type: Number, default: 0 },
-    reviewPoints: { type: Number, default: 0 },
+    review_points: { type: Number, default: 0 },
     username: { type: String },
     language: {
       type: String,
