1f477937bce2ba0e205ee8a13d52473174192ed2
Rostyslav Lisovyi <lisovyi.rostyslav@gmail.com>
2025-11-27 20:37:41 +0100

    Handle unused error param in auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -49,7 +49,7 @@ export const authenticate: RequestHandler = async (
 
     req.auth = buildAuthResult(token, decodedToken);
     next();
-  } catch (error) {
+  } catch (_error) {
     res.status(401).json({ message: "UNAUTHORIZED" });
   }
 };
