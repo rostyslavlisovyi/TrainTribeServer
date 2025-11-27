899bf47e9d241facf23bff950070264b8d39d224
Rostyslav Lisovyi <lisovyi.rostyslav@gmail.com>
2025-11-27 20:45:23 +0100

    Remove unused Request import from express types

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,6 +1,5 @@
 import type { AwilixContainer } from "awilix";
 import type { AuthResult } from "express-oauth2-jwt-bearer";
-import type { Request } from "express";
 
 declare module "express" {
   export interface Request {
